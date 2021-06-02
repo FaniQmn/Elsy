7493b19cf59bdf17f8039cb3fdd2d2106096f666
FaniQmn <QuemonF@gmail.com>
2021-06-02 21:42:22 +0200

    Exercice Elsy

--- src/App.js
+++ src/App.js
@@ -11,14 +11,35 @@ const stepsMin = 0
 const stepsMax = 50000
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    }
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <Box />
-          <p>Heart: {heartMax}
-             Temperature: {tempMin}
-             Steps: {stepsMin}</p>
+          <Box/>
+ 
+          {/* Water */}
+          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
+          
+          {/* Steps */}
+          <Box icon="directions_walk" color="black" value={3000} unit="steps" />
+
+          {/* Heart */}
+          <Box icon="favorite" color="red" value={120} unit="bpm" />
+
+          {/* Temperature */}
+          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" />
+
         </div>  
       </div>
     );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,7 +4,8 @@ class Box extends React.Component {
   render () {
     return (
       <div className="box col-sm-3 col-6">
-        <span class="material-icons">props.color props.icon</span>
+        <span class="material-icons"  style={{fontSize:100, color:this.props.color}}>{this.props.icon}</span>
+        <p>{this.props.unit}{this.props.value}</p>
       </div>
     )
   }
